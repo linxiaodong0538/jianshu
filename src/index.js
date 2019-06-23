72fe00e30ed9c7bafd7cd0b267d0801d2b75d22f
linxin <734127061@qq.com>
2019-06-23 22:18:12 +0800

    图标

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './style';
+// import './static/icon-font/iconfont'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
